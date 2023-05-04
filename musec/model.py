fixed loss calculation

musec/model.py
@@ -10,9 +10,9 @@ from dataclasses import dataclass
 
 @dataclass
 class ModelConfig:
-    d_model: int = 528
-    n_heads: int = 22
-    n_layers: int = 48
+    d_model: int = 8
+    n_heads: int = 2
+    n_layers: int = 2
     ff_mult: int = 4
     drop_p = 0.1
     max_seq_len: int = 2048
@@ -202,6 +202,7 @@ class Transformer(nn.Module):
         hidden_states = self.tok_embeddings(src)
 
         # Slices freqs_cis (pos embeddings) according to src seq_len
+        assert src.shape[1] <= self.model_config.max_seq_len, "Too long."
         freqs_cis = torch.view_as_complex(self.freqs_cis)[: src.shape[1]]
 
         # Implements gradient checkpoints on Encoder Layers.
@@ -263,7 +264,8 @@ class TransformerLM(nn.Module):
                 seq_len, d_model).
 
         Returns:
-            torch.tensor: Forward pass of src through the encoder block.
+            torch.tensor: Forward pass of src through Transformer and LM head.
+                Has shape (batch_size, seq_len, vocab_size).
         """
         logits = self.lm_head(self.model(src))
 

musec/train.py
@@ -22,14 +22,19 @@ class PretrainLM(pl.LightningModule):
         self.save_hyperparameters()
 
         self.model = TransformerLM(model_config)
-        self.loss_fn = nn.CrossEntropyLoss(ignore_index=model_config.pad_id)
+        self.loss_fn = nn.CrossEntropyLoss()  # Not ignoring pad_tok
 
     def forward(self, src: torch.Tensor):
         return self.model(src)
 
     def training_step(self, batch, batch_idx):
-        src, tgt = batch
-        logits = self.model(src).transpose(1, 2)
+        src, tgt = batch  # (b_sz, s_len), (b_sz, s_len, v_sz)
+        logits = self.model(src)  # (b_sz, s_len, v_sz)
+
+        # Transpose for CrossEntropyLoss
+        logits = logits.transpose(1, 2)
+        tgt = tgt.transpose(1, 2)
+
         loss = self.loss_fn(logits, tgt)
 
         self.log(
@@ -45,8 +50,13 @@ class PretrainLM(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        src, tgt = batch
-        logits = self.model(src).transpose(1, 2)
+        src, tgt = batch  # (b_sz, s_len), (b_sz, s_len, v_sz)
+        logits = self.model(src)  # (b_sz, s_len, v_sz)
+
+        # Transpose for CrossEntropyLoss
+        logits = logits.transpose(1, 2)
+        tgt = tgt.transpose(1, 2)
+
         loss = self.loss_fn(logits, tgt).item()
 
         self.log(
@@ -59,7 +69,7 @@ class PretrainLM(pl.LightningModule):
             sync_dist=True,  # Not sure what this does
         )
 
-    def configure_optimizers(self):  # Change to LLAMA
+    def configure_optimizers(self):
         # From LLaMa
         lr = 3e-4
         betas = (0.9, 0.95)